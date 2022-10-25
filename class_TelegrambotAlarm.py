fcfaeea1a246a97599a7603633359321f44bedcb
Ivond <Ivond@yandex.ru>
2022-10-25 21:43:11 +0700

    добавлен метод _parse_message

--- class_TelegrambotAlarm.py
+++ class_TelegrambotAlarm.py
@@ -1,9 +1,10 @@
 #
 #import json
 import sqlite3 
-from class_ThreadSNMPAsc import ThreadSNMPAsk
+import re
+from class_ThreadSNMPAscMain import ThreadSNMPAsk
 from pathlib import Path
-from class_SqlLite import ConnectSqlDB
+from class_SqlLiteMain import ConnectSqlDB
 
 # Проверяет актуальные параметры оборудования по которым есть аваии
 class TelegramBotAlarmStatus(ThreadSNMPAsk):
@@ -49,81 +50,141 @@ class TelegramBotAlarmStatus(ThreadSNMPAsk):
         try:
             with ConnectSqlDB() as sql_db:
                 # Делаем запрос к БД, получить model и описание ГДЕ ip из таблицы Devices
-                model, description = sql_db.get_db('model', 'description', ip=ip, table='Devices')
+                model, host_name = sql_db.get_db('model', 'description', ip=ip, table='Devices')
             # Если model равна
             if model == 'forpost':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.forpost(ip, timeout=5, flag=True)
-                # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Делаем подмену ip адреса на host_name
+                #result = line.replace(line.split()[2], description)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'forpost_2':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.forpost_2(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'forpost_3':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.forpost_3(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'eaton':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.eaton(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'sc200':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.sc200(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
-            elif model == 'megatec':
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
+            elif model == 'legrand':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
-                line = self.megatec(ip, timeout=5, flag=True)
+                line = self.legrand(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'apc':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.apc(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'eltek':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.eltek(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'macc':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.macc(ip, timeout=5, flag=True)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
             elif model == 'modbus':
                 # Вызываем метод и получаем значение параметров, которые записываем в переменную line
                 line = self.modbus(ip, time_out=5)
                 # Делаем подмену ip адреса на description
-                result = line.replace(line.split()[2], description)
-                # Добавляем строку с пораметрами в список status_alarm
-                self.status_alarm.append(result)
+                #result = line.replace(line.split()[2], description)
+                # Вызываем метод _parse_message передаем на вход вывод SNMP запроса, получаем дату и параметры 
+                date, description = self._parse_message(line)
+                # Формируем строку сообщения 
+                massege = f"{date} {host_name} {description}"
+                # Добавляем сообщение в список status_alarm
+                self.status_alarm.append(massege)
         except (sqlite3.IntegrityError, sqlite3.InterfaceError, TypeError, ValueError, IndexError):
             # TODO ошибка запроса к БД
             pass
+
+    def _parse_message(self, line):
+        match = re.match(r'^(?P<date>[\d+\-*]+ +[\d+\:*]+) .+(?P<description>IN.+)', line)
+        match1 = re.match(r'^(?P<date>[\d+\-*]+ +[\d+\:*]+) .+(?P<description>ОПС.+)', line)
+        match2 = re.match(r'^(?P<date>[\d+\-*]+ +[\d+\:*]+) .+(?P<description>TxFiber2.+)', line)
+        # Это условие для ИБЭП
+        if match:
+            date = match.group('date').strip()
+            description = match.group('description').strip()
+            return date, description
+        # Это условия для сообщения приходящего по протоколу ModBus
+        elif match1:
+            date = match1.group('date').strip()
+            description = match1.group('description').strip(' ')
+            return date, description
+        # Это условие для MAC&C
+        elif match2:
+            date = match2.group('date').strip()
+            description = match2.group('description').strip(' ')
+            return date, description
 
 
 if __name__ == '__main__':
